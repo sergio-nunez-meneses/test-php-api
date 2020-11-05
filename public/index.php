271cd7320ac6309eed3470910b9d503b633cc213
sergio núñez meneses <63654322+sergio-nunez-meneses@users.noreply.github.com>
2020-11-05 16:23:21 +0100

    added person controller; optimized index.php

--- public/index.php
+++ public/index.php
@@ -11,34 +11,15 @@ header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode('/', $uri);
 $user_id = null;
+$request_method = $_SERVER['REQUEST_METHOD'];
 
-// route requests
-if ($uri[1] === 'person') {
-  $person_model = new PersonModel();
-
-  if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    if (isset($uri[2])) {
-      $user_id = $uri[2];
-      $response = $person_model->findOne($user_id);
-    } else {
-      $response = $person_model->findAll();
-    }
-  } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $response = $person_model->create($_POST);
-  } elseif ($_SERVER['REQUEST_METHOD'] === 'PUT') {
-    $user_id = $uri[2];
-    parse_str(file_get_contents('php://input') , $output);
-    $response = $person_model->update($user_id, $output);
-  } elseif ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
-    $user_id = $uri[2];
-    $response = $person_model->delete($user_id);
-  }
-
-  if (isset($response) && $response['body']) {
-    header($response['status_code_header']);
-    echo $response['body'];
-  }
-} else {
+if ($uri[1] !== 'person') {
   header('HTTP/1.1 404 Not Found');
   exit();
 }
+
+if (isset($uri[2])) {
+  $user_id = $uri[2];
+}
+
+PersonController::route_request($request_method, $user_id);
