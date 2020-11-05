9aa7490e9b02459d1e82e9f7e4d922590f018bc9
sergio núñez meneses <63654322+sergio-nunez-meneses@users.noreply.github.com>
2020-11-05 11:23:33 +0100

    added findOne method; updated index.php

--- public/index.php
+++ public/index.php
@@ -11,12 +11,24 @@ header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode('/', $uri);
+$userId = null;
 
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+// route requests
+if ($uri[1] === 'person') {
   $personGateway = new PersonGateway($dbConnection);
-  $response = $personGateway->findAll();
+
+  if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    if (isset($uri[2])) {
+      $response = $personGateway->findOne(2);
+    } else {
+      $response = $personGateway->findAll();
+    }
+  }
 
   if ($response['body']) {
     echo $response['body'];
   }
+} else {
+  header('HTTP/1.1 404 Not Found');
+  exit();
 }
--- src/TableGateways/PersonGateway.php
+++ src/TableGateways/PersonGateway.php
@@ -11,6 +11,15 @@ class PersonGateway
     $this->db = $db;
   }
 
+  public function to_json($array = [])
+  {
+    if (count($array) > 0) {
+      $response['status_code_header'] = 'HTTP/1.1 200 OK';
+      $response['body'] = json_encode($array);
+      return $response;
+    }
+  }
+
   public function findAll()
   {
     $sql = "SELECT * FROM person";
@@ -19,13 +28,26 @@ class PersonGateway
     {
       $stmt = $this->db->query($sql);
       $res = $stmt->fetchAll(\PDO::FETCH_ASSOC);
-      $response['status_code_header'] = 'HTTP/1.1 200 OK';
-      $response['body'] = json_encode($res);
-      return $response;
+      return $this->to_json($res);
     }
     catch (\PDOException $e)
     {
       exit($e->getMessage());
     }
+  }
+
+  public function findOne($id)
+  {
+    $sql = "SELECT * FROM person WHERE id =:id";
+
+    try {
+      $stmt = $this->db->prepare($sql);
+      $stmt->execute(['id' => $id]);
+      $res = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+      return $this->to_json($res);
+    } catch (\PDOException $e) {
+      exit($e->getMessage());
+    }
+
   }
 }
