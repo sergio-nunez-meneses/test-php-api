4e424065f4aaf9ac48e20f71afbbd7c6e9ac9dbb
sergio núñez meneses <63654322+sergio-nunez-meneses@users.noreply.github.com>
2020-11-09 14:10:31 +0100

    removed new composer modules; jwt is decoded manually; working on token kid validation

---
+++ public/client.php
@@ -0,0 +1,121 @@
+<?php
+require('../tools/okta.php');
+
+$client_id = OKTACLIENTID;
+$client_secret = OKTASECRET;
+$scope = SCOPE;
+$issuer = OKTAISSUER;
+$curl_options = [];
+$get_token = obtain_token($issuer, $client_id, $client_secret, $scope);
+
+// test requests
+get_all_users($get_token);
+get_user($get_token, 2);
+
+function obtain_token($issuer, $client_id, $client_secret, $scope) {
+  echo "Obtaining token...\n";
+
+  // set up request
+  $uri = $issuer . '/v1/token';
+  $token = base64_encode("$client_id:$client_secret");
+  $payload = http_build_query([
+    'grant_type' => 'client_credentials',
+    'scope' => $scope
+  ]);
+  $curl_opts = [
+    CURLOPT_HTTPHEADER => [
+      'Content-Type: application/x-www-form-urlencoded',
+      "Authorization: Basic $token"
+    ],
+    CURLOPT_POST => 1,
+    CURLOPT_POSTFIELDS => $payload,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_SSL_VERIFYPEER => false, // fiexd bug 'Curl failed with error #60'
+    CURLOPT_VERBOSE => TRUE
+  ];
+
+  // build curl request
+  try {
+    $ch = curl_init($uri);
+
+    if ($ch === false) {
+      throw new \Exception("Failed to initialize request\n");
+    }
+
+    curl_setopt_array($ch, $curl_opts);
+    $response = curl_exec($ch); // process and return response
+
+    if ($response === false) {
+      throw new Exception(curl_error($ch), curl_errno($ch));
+    }
+
+    $response = json_decode($response, true);
+
+    if (!isset($response['access_token']) || !isset($response['token_type'])) {
+      exit("Failed, exiting.\n");
+    }
+
+    echo "Success!\n";
+    // curl_close($ch);
+    return $response['token_type'] . ' ' . $response['access_token'];
+  } catch (\Exception $e) {
+    trigger_error(sprintf(
+        'Curl failed with error #%d: %s',
+        $e->getCode(), $e->getMessage()),
+        E_USER_ERROR);
+  }
+}
+
+function get_all_users($token) {
+  echo "Getting all users...\n";
+  $curl_opts = [
+    CURLOPT_URL => 'http://127.0.0.1:8000/person',
+    CURLOPT_HTTPHEADER => [
+      'Content-Type: application/json',
+      "Authorization: $token"
+    ],
+    CURLOPT_RETURNTRANSFER => true
+  ];
+
+  try {
+    $ch = curl_init();
+
+    if ($ch === false) {
+      throw new \Exception("Failed to initialize request\n");
+    }
+
+    curl_setopt_array($ch, $curl_opts);
+    $response = curl_exec($ch);
+
+    var_dump($response);
+  } catch (\Exception $e) {
+    echo $e->getMessage();
+  }
+}
+
+function get_user($token, $id) {
+  echo "Getting user id#$id...\n";
+  $curl_opts = [
+    CURLOPT_URL => "http://127.0.0.1:8000/person/$id",
+    CURLOPT_HTTPHEADER => [
+      'Content-Type: application/json',
+      "Authorization: $token"
+    ],
+    CURLOPT_RETURNTRANSFER => true
+  ];
+
+  try {
+    $ch = curl_init();
+
+    if ($ch === false) {
+      throw new \Exception("Failed to initialize request\n");
+    }
+
+    curl_setopt_array($ch, $curl_opts);
+    $response = curl_exec($ch);
+
+    var_dump($response);
+  } catch (\Exception $e) {
+    echo $e->getMessage();
+  }
+}
--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
 require('../bootstrap.php');
+require('../tools/okta.php');
+// use \Okta\JwtVerifier\JwtVerifierBuilder;
 use Src\Controller\PersonController;
 
 // handle Cross origin resource sharing (CORS)
@@ -23,4 +25,62 @@ if (isset($uri[2])) {
   $user_id = $uri[2];
 }
 
+// request authentication with okta
+if (!authenticate()) {
+  header('HTTP/1.1 401 Unauthorized');
+  exit('Unauthorized');
+}
+
 PersonController::route_request($dbConnection, $request_method, $user_id);
+
+function authenticate() {
+  switch (true) {
+    case array_key_exists('HTTP_AUTHORIZATION', $_SERVER):
+      $auth_header = $_SERVER['HTTP_AUTHORIZATION'];
+      break;
+
+    case array_key_exists('Authorization', $_SERVER):
+      $auth_header = $_SERVER['Authorization'];
+      break;
+
+    default:
+      $auth_header = null;
+      break;
+  }
+
+  preg_match('/Bearer\s(\S+)/', $auth_header, $matches);
+
+  if (!isset($matches[1])) {
+    throw new \Exception('No Bearer Token');
+  }
+
+  // after installing different modules suggested in the following: error Uncaught Exception: Could not discover JWT Library, Please make sure one is included and the Adaptor is used in path\to\project\vendor\okta\jwt-verifier\src\Adaptors\AutoDiscover.php:34, JwtVerifierBuilder stopped working 
+
+  // $jwt = (new \Okta\JwtVerifier\JwtVerifierBuilder())
+  //   ->setIssuer(OKTAISSUER)
+  //   ->setAudience(OKTAAUDIENCE)
+  //   ->setClientId(OKTACLIENTID)
+  //   ->build();
+
+  try {
+    // var_dump($jwt->verify($matches[1]));
+    list($header, $payload, $signature) = explode('.', $matches[1]);
+
+    $plainHeader = base64_decode($header);
+    echo "Header:\n$plainHeader\n\n";
+
+    $plainPayload = base64_decode($payload);
+    echo "Payload:\n$plainPayload\n\n";
+
+    $plainSignature = base64_decode($signature);
+    echo "Signature:\n$signature\n\n";
+
+    $kid = json_decode($plainHeader, true);
+    echo "kid:\n" . $kid['kid'] . "\n\n";
+
+    // return $jwt->verify($matches[1]);
+    return true;
+  } catch (\Exception $e) {
+    return false;
+  }
+}
